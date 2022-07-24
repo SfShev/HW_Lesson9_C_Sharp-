a12e70a0bd152ec42373008d1a0b9bfb18fd4faf
SfShev <pshev1406@gmail.com>
2022-07-24 20:57:29 +0300

    Сумма натуральных чисел в промежутке от N до М (Рекурсия)

---
+++ task_64/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Задайте начало и конец промежутка,в котором вы хотите унать сумму чисел");
+Console.Write("Задайте число начала счета: ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте число конца счета: ");
+int over = Convert.ToInt32(Console.ReadLine());
+
+int SumNum(int start, int over)
+{
+    int sum =0;
+    if (start > over) return 0;
+    else return sum += over + SumNum(start, over - 1);
+}
+
+void PrintSumNum()
+{
+Console.WriteLine($"Сумма чисел в промежутке от {start} до {over} = {SumNum(start, over)}");
+}
+
+PrintSumNum();
